2a07acad759fea6a899c0c3301cbaeb84d2c5e2a
yuuhana <yuu.quoi@gmail.com>
2025-10-29 09:05:20 +0800

    tool state

--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -1,4 +1,6 @@
 class_name Player
 extends CharacterBody2D
 
+@export var current_tool: DataTypes.Tools = DataTypes.Tools.None
+
 var player_direction: Vector2
--- scripts/game_input_events.gd
+++ scripts/game_input_events.gd
@@ -22,3 +22,8 @@ static func is_movement_input() -> bool:
         return false
     else:
         return true
+
+static func use_tool() -> bool:
+    var use_tool_value: bool = Input.is_action_just_pressed("hit")
+
+    return use_tool_value
