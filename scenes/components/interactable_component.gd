cc3520092419d145d31e1193211b445a9db2bcab
yuuhana <yuu.quoi@gmail.com>
2025-10-30 13:22:33 +0800

    no using param change name

--- scenes/components/interactable_component.gd
+++ scenes/components/interactable_component.gd
@@ -5,8 +5,8 @@ signal interactable_activated
 signal interactable_deactivated
 
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body: Node2D) -> void:
     interactable_activated.emit()
 
-func _on_body_exited(body: Node2D) -> void:
+func _on_body_exited(_body: Node2D) -> void:
     interactable_deactivated.emit()
