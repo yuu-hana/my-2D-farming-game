56ef9aa812f8ee021488c065bcee29bcf494c49e
yuuhana <yuu.quoi@gmail.com>
2025-10-30 15:59:44 +0800

    create trees

---
+++ scenes/components/collectable_component.gd
@@ -0,0 +1,8 @@
+class_name CollectableComponent
+extends Area2D
+
+@export var collectable_name: String
+
+func _on_body_entered(body:Node2D) -> void:
+    if body is Player:
+        get_parent().queue_free()
---
+++ scenes/components/hurt_component.gd
@@ -0,0 +1,12 @@
+class_name HurtComponent
+extends Area2D
+
+@export var tool : DataTypes.Tools = DataTypes.Tools.None
+
+signal on_hurt
+
+func _on_area_entered(_area:Area2D) -> void:
+    var hit_component = _area as HitComponent
+
+    if tool == hit_component.current_tool:
+        on_hurt.emit(hit_component.hit_damage)
