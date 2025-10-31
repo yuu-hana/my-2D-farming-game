efbc23e61a1ec4fa589660d48a4a750159d3e2f7
yuuhana <yuu.quoi@gmail.com>
2025-10-31 17:27:57 +0800

    create chicken w/ navigation agent

--- scenes/objects/rocks/rock.gd
+++ scenes/objects/rocks/rock.gd
@@ -6,20 +6,20 @@ extends Sprite2D
 var stone_scene = preload("res://scenes/objects/rocks/stone.tscn")
 
 func _ready() -> void:
-	hurt_component.on_hurt.connect(on_hurt)
-	damage_component.max_damage_reached.connect(on_max_damage_reached)
+    hurt_component.on_hurt.connect(on_hurt)
+    damage_component.max_damage_reached.connect(on_max_damage_reached)
 
 func on_hurt(hit_damage: int) -> void:
-	damage_component.apply_damage(hit_damage)
-	material.set_shader_parameter("shake_intensity", 1.0)
-	await get_tree().create_timer(1.0).timeout
-	material.set_shader_parameter("shake_intensity", 0.0)
+    damage_component.apply_damage(hit_damage)
+    material.set_shader_parameter("shake_intensity", 1.0)
+    await get_tree().create_timer(1.0).timeout
+    material.set_shader_parameter("shake_intensity", 0.0)
 
 func on_max_damage_reached() -> void:
-	call_deferred("add_stone_scene")
-	queue_free()
+    call_deferred("add_stone_scene")
+    queue_free()
 
 func add_stone_scene() -> void:
-	var stone_instance = stone_scene.instantiate() as Node2D
-	stone_instance.global_position = global_position
-	get_parent().add_child(stone_instance)
+    var stone_instance = stone_scene.instantiate() as Node2D
+    stone_instance.global_position = global_position
+    get_parent().add_child(stone_instance)
